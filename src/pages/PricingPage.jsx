ace786ed7db9940346fe6e1650d49b362e50d79c
Joan Sisson <sissonlegalservices@aol.com>
2025-07-13 17:45:48 -0500

    Create PricingPage.jsx

---
+++ src/pages/PricingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PricingSection from '../components/marketing/PricingSection';
+
+const PricingPage = () => {
+  return (
+    <div className="container">
+      <PricingSection />
+    </div>
+  );
+};
+
+export default PricingPage;
