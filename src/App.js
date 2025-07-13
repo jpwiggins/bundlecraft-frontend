8276d1d6b10120822c60eef21741cc23297d8ab6
Joan Sisson <sissonlegalservices@aol.com>
2025-07-13 17:32:43 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import { BundleProvider } from './contexts/BundleContext';
-import Dashboard from './pages/Dashboard';
-import AuthPage from './pages/AuthPage';
+import Layout from './components/layout/Layout';
 import HomePage from './pages/HomePage';
-import './App.css';
+import AuthPage from './pages/AuthPage';
+import DashboardPage from './pages/DashboardPage';
+import PricingPage from './pages/PricingPage';
+import './index.css';
 
 function App() {
   return (
@@ -13,9 +15,12 @@ function App() {
       <AuthProvider>
         <BundleProvider>
           <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/login" element={<AuthPage />} />
-            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/" element={<Layout />}>
+              <Route index element={<HomePage />} />
+              <Route path="login" element={<AuthPage />} />
+              <Route path="pricing" element={<PricingPage />} />
+              <Route path="dashboard" element={<DashboardPage />} />
+            </Route>
           </Routes>
         </BundleProvider>
       </AuthProvider>
